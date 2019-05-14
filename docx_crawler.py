d544e49417891dd1358c888562a2c19c0108c93c
Manoj Thoranala <manojthoranala@gmail.com>
2019-05-14 13:38:42 +0530

    docx_crawler_initial

---
+++ docx_crawler.py
@@ -0,0 +1,13 @@
+"""/*H******************************************************************************************************************
+* FILENAME : automate.py
+*
+* DESCRIPTION : The script is automates the task for word document using python_docx library 
+# for library refer - https://pypi.org/project/python-docx
+*
+* NOTES :
+*
+* AUTHOR :    Manoj Manjunatha        START DATE :    10 July 2018
+*
+*H*/"""
+
+import docx
